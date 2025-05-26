92f30383b2c731aaa1b591e8ea0a903b0a74c707
ChanaVatury <cv3285191@gmail.com>
2025-05-26 23:52:25 +0300

    fix

--- server/SalaryCalculator.API/SalaryCalculator.API/BL/SalaryService.cs
+++ server/SalaryCalculator.API/SalaryCalculator.API/BL/SalaryService.cs
@@ -48,9 +48,6 @@ namespace SalaryCalculator.API.Services
                 case ProfessionalLevelEnum.Experienced:
                     baseHourlyRate = 120; // שכר יסוד לשעה למנוסה 
                     break;
-                default:
-                    baseHourlyRate = 100; // ברירת מחדל
-                    break;
             }
             // שליפת כל אחוזי העסקה בכדי להשתמש באחוז עצמו 
             var allPercents = await GetPartTimePercentsAsync();
@@ -59,7 +56,7 @@ namespace SalaryCalculator.API.Services
             var allManagementLevel = await GetManagementLevelsAsync();
             var managementLeve = allManagementLevel.FirstOrDefault(p => p.Id == (int)input.ManagementLevel);
 
-            // 2. הוספת תוספת לרמה ניהולית (20 ש"ח לכל רמה)
+            // 2. הוספת תוספת לרמה ניהולית - 20 ש"ח לכל רמה
             int managementLevelNumber = managementLeve.LevelValue;
             baseHourlyRate += managementLevelNumber * 20;
 
@@ -67,11 +64,11 @@ namespace SalaryCalculator.API.Services
             //decimal salaryBase = baseHourlyRate * baseHoursPerMonth * ((int)input.PartTimePercent / 100m);
             decimal salaryBase = baseHourlyRate * baseHoursPerMonth * (partTimePercent.Percent / 100m);
 
-            // 4. חישוב תוספת ותק (1.25% לכל שנת ותק)
+            // 4. חישוב תוספת ותק - 1.25% לכל שנת ותק
             decimal seniorityBonusPercent = 1.25m * (int)input.SeniorityYears;
             decimal seniorityBonusAmount = salaryBase * (seniorityBonusPercent / 100m);
 
-            // 5. חישוב תוספת עבודה בחוק (1% לקבוצה א', 0.5% לקבוצה ב')
+            // 5. חישוב תוספת עבודה בחוק - 1% לקבוצה א', 0.5% לקבוצה ב
             decimal lawBonusAmount = 0;
             if (input.LawBonusEligible)
             {
@@ -122,104 +119,6 @@ namespace SalaryCalculator.API.Services
 
             return await Task.FromResult(result);
         }
-
-        //public async Task<SalaryCalculationResult> CalculateSalaryAsync(SalaryCalculationInput input)
-        //{
-        //    // ---------------
-
-        //    // 1. חישוב שכר יסוד לפי חלקיות משרה, רמה מקצועית וניהולית
-        //    // 2. חישוב תוספת וותק, תוספת עבודה בחוק
-        //    // 3. חישוב תוספת העלאה לפי שכר בסיס ורמה ניהולית
-        //    // 4. החזרת התוצאה עם כל הפרטים
-
-        //    // ---------------
-
-        //    // דוגמה של חישוב לפי הנתונים שלך
-
-        //    const int baseHoursPerMonth = 170;
-        //    decimal baseHourlyRate = 0;
-
-        //    // שכר יסוד לפי רמה מקצועית
-        //    switch (input.ProfessionalLevelName)
-        //    {
-        //        case "מתחיל":
-        //            baseHourlyRate = 100;
-        //            break;
-        //        case "מנוסה":
-        //            baseHourlyRate = 120;
-        //            break;
-        //        default:
-        //            baseHourlyRate = 100;
-        //            break;
-        //    }
-
-        //    // הוספת תשלום לפי רמה ניהולית
-        //    int managementLevelNumber = 0;
-        //    if (input.ManagementLevelName != "ללא" && int.TryParse(input.ManagementLevelName.Replace("רמת ניהול ", ""), out int level))
-        //    {
-        //        managementLevelNumber = level;
-        //    }
-        //    else if (input.ManagementLevelName == "ללא")
-        //    {
-        //        managementLevelNumber = 0;
-        //    }
-
-        //    baseHourlyRate += managementLevelNumber * 20;
-
-        //    // קבלת אחוז משרה
-        //    int percentPartTime = input.PartTimePercent;
-
-        //    // חישוב שכר יסוד לפי חלקיות משרה
-        //    decimal salaryBase = baseHourlyRate * baseHoursPerMonth * (percentPartTime / 100m);
-
-        //    // חישוב תוספת וותק (1.25% לשנה)
-        //    decimal seniorityBonusPercent = 1.25m * input.SeniorityYears;
-        //    decimal seniorityBonusAmount = salaryBase * (seniorityBonusPercent / 100m);
-
-        //    // חישוב תוספת עבודה בחוק
-        //    decimal lawBonusAmount = 0;
-        //    if (input.LawBonusEligible)
-        //    {
-        //        if (input.LawBonusGroupName == "קבוצה א'")
-        //            lawBonusAmount = salaryBase * 0.01m; // 1%
-        //        else if (input.LawBonusGroupName == "קבוצה ב'")
-        //            lawBonusAmount = salaryBase * 0.005m; // 0.5%
-        //    }
-
-        //    // שכר בסיס כולל תוספות לפני העלאה
-        //    decimal salaryBaseBeforeRaise = salaryBase + seniorityBonusAmount + lawBonusAmount;
-
-        //    // חישוב תוספת העלאה
-        //    decimal raisePercent = 0;
-        //    if (salaryBaseBeforeRaise <= 20000)
-        //        raisePercent = 1.5m;
-        //    else if (salaryBaseBeforeRaise <= 30000)
-        //        raisePercent = 1.25m;
-        //    else
-        //        raisePercent = 1.0m;
-
-        //    // הוספת 0.1% לכל רמת ניהול לתוספת
-        //    raisePercent += managementLevelNumber * 0.1m;
-
-        //    decimal raiseAmount = salaryBaseBeforeRaise * (raisePercent / 100m);
-
-        //    // שכר בסיס לאחר העלאה
-        //    decimal salaryBaseAfterRaise = salaryBaseBeforeRaise + raiseAmount;
-
-        //    // יצירת אובייקט תוצאה
-        //    var result = new SalaryCalculationResult
-        //    {
-        //        BaseSalary = salaryBase,
-        //        SeniorityBonusPercent = seniorityBonusPercent,
-        //        SeniorityBonusAmount = seniorityBonusAmount,
-        //        LawBonusAmount = lawBonusAmount,
-        //        SalaryBeforeRaise = salaryBaseBeforeRaise,
-        //        RaisePercent = raisePercent,
-        //        RaiseAmount = raiseAmount,
-        //        SalaryAfterRaise = salaryBaseAfterRaise
-        //    };
-
-        //    return await Task.FromResult(result);
-        //}
+        
     }
 }
--- server/SalaryCalculator.API/SalaryCalculator.API/Models/SalaryCalculationInput.cs
+++ server/SalaryCalculator.API/SalaryCalculator.API/Models/SalaryCalculationInput.cs
@@ -1,12 +1,17 @@
 ﻿using SalaryCalculator.API.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalaryCalculator.API.Models
 {
     public class SalaryCalculationInput
     {
+        [Required]
         public PartTimePercentEnum PartTimePercent { get; set; }
+        [Required]
         public ProfessionalLevelEnum ProfessionalLevel { get; set; }
+        [Required]
         public ManagementLevelEnum ManagementLevel { get; set; }
+        [Required]
         public decimal SeniorityYears { get; set; }
         public bool LawBonusEligible { get; set; }
         public LawBonusGroupEnum? LawBonusGroup { get; set; } // אם !LawBonusEligible => None
